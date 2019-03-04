1cca625d5d832c06471853d84ba55cd86bbd6945
Toni <toni.pafe@gmail.com>
2019-03-04 18:13:21 +0100

    Created a new class Product to store the data.

---
+++ webscaper/product.py
@@ -0,0 +1,16 @@
+#!/user/bin/env python3
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf8
+
+
+class Product(object):
+
+    def __init__(self, title, new_price, old_price):
+        object.__init__(self)
+        self.title = title
+        self.new_price = new_price
+        self.old_price = old_price
+
+    def __str__(self):
+        return "Title: {}\n New price: {}\n Old price {}\n"\
+            .format(self.title, self.new_price, self.old_price)
